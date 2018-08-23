3036214b670c6d9a5cb74d82d56acdb35cff7299
Maximilian Kurthen <maxk@prelude.MPA-Garching.MPG.DE>
2018-08-23 07:21:01 +0200

    move to github commit #2

---
+++ README
@@ -0,0 +1,45 @@
+# Prerequisits
+    -Python 3.6.6
+    -Colorama 0.3.9
+    -Numpy 1.15.0
+    -Scikit-Learn 0.19.1
+    -SciPy 1.1.0
+
+to use the NIFTy implementation:
+    -NIFTy 5
+
+the Jupyter notebooks havee been created using
+    -jupyter 1.0.0
+
+the Plots have been done using
+    -matplotlib 2.2.3
+    -seaborn 0.9.0
+
+
+# Benchmarks
+
+to perform a benchmark, call the script do_benchmark.py via:
+  python do_benchmark.py --args
+  
+where args refer to:
+  --benchmark: one of the supported benchmarks, either a "bcs" 
+        (bayesian causal sampling) benchmark, these are stored
+        in the './benchmarks' folder and use the formatting as
+        in the comparative study by Mooij16
+  --model: currently either 1 or 2, 1 refers to the shallow model
+        implemented via NumPy, 2 uses the same model implemented
+        in NIFTy
+  --nvar: value for the noise variance, 1e-2 has shown to provide
+        good results
+  --nbins: number of bins for the Poissonian discretization and 
+        for the field approximation. 512 performs well here
+  --power_spectrum_beta: the P_beta power spectrum. This has to be
+        given in the format such that the string 
+        ("lambda q: " + power_spectrum_beta) can be parsed to a
+        valid lambda expression, e.g. "2048/(q**4 + 1)"
+  --power_spectrum_f: the P_f power spectrum. Same formmating as
+        above
+  --rho: Value for rho in the inference model.
+  --scale_max: all x and y data will be scaled to the interval 
+      [0, scale_max]
+    
--- generate_data_scripts/generate_data_30samples.py
+++ generate_data_scripts/generate_data_30samples.py
@@ -6,7 +6,9 @@ import sys
 sys.path.append('..')
 import bayesian_causal_model.bayesian_causal_sampling_numpy
 
-BENCHMARK_ROOT = '/afs/mpa/home/maxk/bayesian_causal_inference_benchmarks'
+BENCHMARK_FOLDER = (
+    '/afs/mpa/home/maxk/bayesian_causal_inference/benchmarks/bcs_30samples/')
+
 
 np.random.seed(20)
 
@@ -16,31 +18,31 @@ p_spec_f = lambda q: 16/(q**4 + 1)
 for i in range(100):
     np.random.seed(i)
     bcs = bayesian_causal_model.bayesian_causal_sampling_numpy.BayesianCausalSampler(
-        N_bins=16,
+        N_bins=512,
         power_spectrum_beta=p_spec_beta,
         power_spectrum_f=p_spec_f,
         noise_var=5e-2)
 
     bcs.draw_sample_fields()
-    x, y = bcs.get_samples(300, discretize=True)
+    x, y = bcs.get_samples(30, discretize=True)
     scaler = MinMaxScaler(feature_range=(0, 1))
     x, y = scaler.fit_transform(np.array((x, y)).T).T
     # flip x and y with probability 1/2
     flip = bool(np.random.binomial(1, 0.5))
     if flip:
         np.savetxt(
-                (BENCHMARK_ROOT +
-                'bcs_16bins/pair0{:03d}.txt'.format(i+1)),
+                (BENCHMARK_FOLDER +
+                'pair0{:03d}.txt'.format(i+1)),
                 np.array([y, x]).T, delimiter=' ')
 
-        with open(BENCHMARK_ROOT + 'bcs_16bins/pairmeta.txt', 'a') as f:
+        with open(BENCHMARK_FOLDER + 'pairmeta.txt', 'a') as f:
             f.write('0{:03d} 2 2 1 1 1\n'.format(i+1))
     else:
         np.savetxt(
-                (BENCHMARK_ROOT +
-                'bcs_16bins/pair0{:03d}.txt'.format(i+1)),
+                (BENCHMARK_FOLDER +
+                'pair0{:03d}.txt'.format(i+1)),
                 np.array([x, y]).T, delimiter=' ')
-        with open(BENCHMARK_ROOT + 'bcs_16bins/pairmeta.txt', 'a') as f:
+        with open(BENCHMARK_FOLDER + 'pairmeta.txt', 'a') as f:
             f.write('0{:03d} 1 1 2 2 1\n'.format(i+1))
 
 
