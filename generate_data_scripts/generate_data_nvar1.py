3134e4c93a7e2c1aa8a1d11b6d350ae74ea749d5
Maximilian Kurthen <maxk@prelude.MPA-Garching.MPG.DE>
2018-08-27 08:44:53 +0200

    added generate_data scripts

---
+++ generate_data_scripts/generate_data_nvar1.py
@@ -0,0 +1,45 @@
+import numpy as np
+
+from sklearn.preprocessing import MinMaxScaler
+
+import sys
+sys.path.append('..')
+import bayesian_causal_model.bayesian_causal_sampling_numpy
+
+BENCHMARK_FOLDER = (
+    '/afs/mpa/home/maxk/bayesian_causal_inference/benchmarks/bcs_nvar1')
+
+power_spectrum_beta = lambda q: 512/(q**4 + 1)
+power_spectrum_f = lambda q: 512/(q**4 + 1)
+
+for i in range(100):
+    np.random.seed(i)
+    bcs = bayesian_causal_model.bayesian_causal_sampling_numpy.BayesianCausalSampler(
+        N_bins=512,
+        power_spectrum_beta=power_spectrum_beta,
+        power_spectrum_f=power_spectrum_f,
+        noise_var=1)
+
+    bcs.draw_sample_fields()
+
+    x, y = bcs.get_samples(300)
+    scaler = MinMaxScaler(feature_range=(0, 1))
+    x, y = scaler.fit_transform(np.array((x, y)).T).T
+    # flip x and y with probability 1/2
+    flip = bool(np.random.binomial(1, 0.5))
+    if flip:
+        np.savetxt(
+                (BENCHMARK_FOLDER +
+                'pair0{:03d}.txt'.format(i+1)),
+                np.array([y, x]).T, delimiter=' ')
+
+        with open(BENCHMARK_FOLDER + 'pairmeta.txt', 'a') as f:
+            f.write('0{:03d} 2 2 1 1 1\n'.format(i+1))
+    else:
+        np.savetxt(
+                (BENCHMARK_FOLDER +
+                'pair0{:03d}.txt'.format(i+1)),
+                np.array([x, y]).T, delimiter=' ')
+        with open(BENCHMARK_FOLDER + 'pairmeta.txt', 'a') as f:
+            f.write('0{:03d} 1 1 2 2 1\n'.format(i+1))
+
