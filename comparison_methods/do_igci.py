4cd26e736a9b483a27cc3b6e8bf0cf96678896de
Maximilian Kurthen <maxk@prelude.MPA-Garching.MPG.DE>
2018-08-28 13:19:32 +0200

    added LiNGAM and IGCI scripts

--- comparison_methods/do_igci.py
+++ comparison_methods/do_igci.py
@@ -1,6 +1,5 @@
 import sys
 
-import numpy as np
 import matlab.engine
 
 CAUSALITY_ROOT = '/afs/mpa/home/maxk/bayesian_causal_inference/'
@@ -18,44 +17,38 @@ if LAST_ID is None:
     LAST_ID = get_benchmark_default_length(BENCHMARK)
 
 eng = matlab.engine.start_matlab()
-eng.addpath(CAUSALITY_ROOT + 'comparison_methods/LiNGAM/lingam-1.4.2/code/')
-eng.addpath(
-        CAUSALITY_ROOT + 'comparison_methods/LiNGAM/lingam-1.4.2/FastICA_23/')
+eng.addpath(CAUSALITY_ROOT + 'comparison_methods/Mooij16/cep')
+eng.startup(nargout=0)
+eng.local_config(nargout=0)
 
-# prediction_file = './benchmark_predictions/{}_{}.txt'.format(BENCHMARK, NAME)
-# if os.path.isfile(prediction_file):
-#     c = 0
-#     while os.path.isfile(prediction_file):
-#         c += 1
-#         prediction_file = './benchmark_predictions/{}_{}_{}.txt'.format(
-#                 BENCHMARK, NAME, c)
+methodpars = eng.struct()
+methodpars['ref_measure'] = 2
+methodpars['estimator'] = 'org_entropy'
+methodpars['entest'] = ''
 
 accuracy = 0
+undecided = 0
 sum_of_weights = 0
 weighted_correct = 0
-undecided = 0
 
 for i in range(FIRST_ID-1, LAST_ID):
     (x, y), true_direction, weight = get_pair(i, BENCHMARK)
     if true_direction == 0:
         continue
 
-    x_ = matlab.double(y.tolist())
-    y_ = matlab.double(x.tolist())
-    d = eng.vertcat(x_, y_)
+    x_ = matlab.double(x.tolist())
+    x_T = eng.transpose(x_)
+    y_ = matlab.double(y.tolist())
+    y_T = eng.transpose(y_)
 
-    [B, stde, ci, k] = eng.estimate(d, nargout=4)
-    k = np.array(k).reshape(-1)
-    print(k)
-    if k[0] < k[1]:
-        predicted_direction = 1
-    elif k[0] > k[1]:
-        predicted_direction = -1
-    else:
-        predicted_direction = 0
-        undecided += 1
+    result = eng.cep_igci(x_T, y_T, methodpars)
+    predicted_direction = result['decision']
 
     weighted_correct += weight*(predicted_direction == true_direction)
+
+    if predicted_direction == 0:
+        undecided += 1
+
     sum_of_weights += weight
     accuracy = weighted_correct / sum_of_weights
 
@@ -64,4 +57,5 @@ for i in range(FIRST_ID-1, LAST_ID):
             'accuracy so far: {:.2f}'.format(
                 i, true_direction, predicted_direction, accuracy))
 
-print('accuracy: {:.2f}'.format(accuracy))
+
+print('accuracy: {:.2f}, undecided: {}'.format(accuracy, undecided))
