246ce1c7a8ab017da48eb6f7bb60e51b6fb833e7
Maximilian Kurthen <maxk@prelude.MPA-Garching.MPG.DE>
2018-08-28 10:42:09 +0200

    fixed error in output folder

--- generate_data_scripts/generate_data_nvar1.py
+++ generate_data_scripts/generate_data_nvar1.py
@@ -7,7 +7,7 @@ sys.path.append('..')
 import bayesian_causal_model.bayesian_causal_sampling_numpy
 
 BENCHMARK_FOLDER = (
-    '/afs/mpa/home/maxk/bayesian_causal_inference/benchmarks/bcs_nvar1')
+    '/afs/mpa/home/maxk/bayesian_causal_inference/benchmarks/bcs_nvar1/')
 
 power_spectrum_beta = lambda q: 512/(q**4 + 1)
 power_spectrum_f = lambda q: 512/(q**4 + 1)
@@ -28,18 +28,17 @@ for i in range(100):
     # flip x and y with probability 1/2
     flip = bool(np.random.binomial(1, 0.5))
     if flip:
-        np.savetxt(
-                (BENCHMARK_FOLDER +
+        np.savetxt((
+                BENCHMARK_FOLDER +
                 'pair0{:03d}.txt'.format(i+1)),
                 np.array([y, x]).T, delimiter=' ')
 
         with open(BENCHMARK_FOLDER + 'pairmeta.txt', 'a') as f:
             f.write('0{:03d} 2 2 1 1 1\n'.format(i+1))
     else:
-        np.savetxt(
-                (BENCHMARK_FOLDER +
+        np.savetxt((
+                BENCHMARK_FOLDER +
                 'pair0{:03d}.txt'.format(i+1)),
                 np.array([x, y]).T, delimiter=' ')
         with open(BENCHMARK_FOLDER + 'pairmeta.txt', 'a') as f:
             f.write('0{:03d} 1 1 2 2 1\n'.format(i+1))
-
